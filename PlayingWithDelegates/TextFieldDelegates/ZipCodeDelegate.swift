17cf5a3b2e4ad85bc5449459ef0e234e7c70ae93
Jasmeet Singh <jasmeetsinghbhatia@gmail.com>
2018-12-29 16:27:30 -0500

    Project- PlayingWithDelegates, complete app

---
+++ PlayingWithDelegates/TextFieldDelegates/ZipCodeDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  ZipCodeDelegate.swift
+//  PlayingWithDelegates
+//
+//  Created by Jasmeet Singh on 2018-12-29.
+//  Copyright © 2018 Jusmyth. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ZipCodeDelegate: NSObject, UITextFieldDelegate {
+    
+    func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
+        var newText = textField.text! as NSString
+        newText = newText.replacingCharacters(in: range, with: string) as NSString
+        
+        return newText.length <= 5
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        
+        return true;
+    }
+}
--- PlayingWithDelegates/ViewController.swift
+++ PlayingWithDelegates/ViewController.swift
@@ -10,11 +10,37 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var zipCodeTextField: UITextField!
+    @IBOutlet weak var moneyTextField: UITextField!
+    @IBOutlet weak var toggleTextField: UITextField!
+    @IBOutlet weak var toggleSwitch: UISwitch!
+    
+    let zipCodeDelegate = ZipCodeDelegate()
+    let moneyDelegate = CurrencyDelegate()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.zipCodeTextField.delegate = zipCodeDelegate
+        self.moneyTextField.delegate = moneyDelegate
+        
+        toggleSwitch.setOn(false, animated: false)
+        toggleTextField.placeholder = "Flip switch to enable typing"
     }
 
-
+    @IBAction func toggleSwitchAction(_ sender: UISwitch) {
+        if sender.isOn {
+            toggleTextField.backgroundColor = UIColor.white
+            toggleTextField.isUserInteractionEnabled = true
+            toggleTextField.placeholder = "Typing Enabled, go ahead!"
+        }
+        else {
+            toggleTextField.backgroundColor = UIColor.red
+            toggleTextField.isUserInteractionEnabled = false
+            toggleTextField.text = ""
+            toggleTextField.placeholder = "Flip switch to enable typing"
+        }
+    }
+    
 }
 
